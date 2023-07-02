55e490f923a2949a5248e0f82d8ab3b03aae4fd1
writer-mng <volodymyrpetriv2207@gmail.com>
2023-07-02 21:33:59 +0300

    Base config. Started work on bot

---
+++ bot/config.py
@@ -0,0 +1,11 @@
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = Bot(token=os.getenv('TOKEN'))
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
---
+++ bot/run.py
@@ -0,0 +1,10 @@
+from config import dp, bot
+from aiogram import executor
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.reply("Hello")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
