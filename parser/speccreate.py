07fa6c3e58f1a3cfe114495e06028e61cc2d3e24
Igor4er <sherednihenko.ihor@gmail.com>
2023-07-02 21:33:20 +0300

    Worktime push

---
+++ parser/speccreate.py
@@ -0,0 +1,12 @@
+
+
+
+def list_specs() -> list:
+    with open("speclist.txt", "r") as f:
+        specs = []
+        for line in f.readlines():
+            if 
+
+
+def create_specs():
+    pass
