60ffc48e2f08f2912b0986ed665345c4e9d37ecf
Igor4er <sherednihenko.ihor@gmail.com>
2023-07-02 22:43:24 +0300

    Worktime push

--- vb2/parser/speccreate.py
+++ vb2/parser/speccreate.py
@@ -1,12 +1,22 @@
-
+from vb2.parser.supabase import insert_category_into_base
+from tqdm import tqdm
 
 
 def list_specs() -> list:
     with open("speclist.txt", "r") as f:
         specs = []
         for line in f.readlines():
-            if 
+            if not line[0].isdigit():
+                if len(line) > 5:
+                    specs.append(line)
+        return specs
 
 
 def create_specs():
-    pass
+    spec_list = list_specs()
+    for spec in tqdm(spec_list):
+        insert_category_into_base(spec)
+
+
+if __name__ == "__main__":
+    create_specs()
--- vb2/settings.py
+++ vb2/settings.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 
 class Settings(BaseSettings):
     TOKEN: str = "DEFAULT"
-    BASE_KEY: str = "DEFAULT"
+    DB_KEY: str = "DEFAULT"
+    DB_URL: str = "https://tqaqvhepgpsjyetgpzwo.supabase.co/rest/v1"
     
     class Config:
         env_prefix: str = "VB_"
